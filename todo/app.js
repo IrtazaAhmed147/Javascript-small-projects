ad804c8447150e873e562caf584108eb1bd8b3a5
Irtaza Ahmed <irtazaahmedk@gmail.com>
2024-12-19 13:03:49 +0500

    grocery bud todo list

---
+++ todo/app.js
@@ -0,0 +1,62 @@
+let userInput = document.getElementById('input')
+let btn = document.getElementById('btn')
+let list = document.getElementById('list')
+let todoList = []
+let editId = null
+
+function renderList() {
+    list.innerHTML = ''
+    todoList?.forEach((todo) => {
+        let li = document.createElement('li')
+        li.setAttribute('class', 'todo')
+        li.innerHTML = ` <p>${todo.todo}</p>
+                    <div>
+                        <button class="editBtn" onClick={editFunc(${todo.id})}><i class="fa-solid fa-pen-to-square"></i></button>
+                        <button class="deleteBtn" onClick={delFunc(${todo.id})}><i class="fa-solid fa-trash"></i></button>
+                    </div>`
+        list.appendChild(li)
+    })
+}
+
+btn.addEventListener('click', function () {
+
+    if (btn.innerHTML === 'Add') {
+
+        if (userInput.value.trim()) {
+            list.innerHTML = ''
+            todoList.push({
+                todo: userInput.value,
+                id: Date.now(),
+            })
+            userInput.value = ''
+            renderList()
+        }
+    } else if (btn.innerHTML === 'Edit') {
+        let todo = todoList.find((todo) => todo.id === editId)
+        if (todo) {
+            todo.todo = userInput.value;
+            renderList();
+            btn.innerHTML = 'Add';
+            userInput.value = '';
+            editId = null; 
+        }
+
+    }
+
+})
+
+function delFunc(id) {
+    todoList = todoList.filter((todo) => {
+        return todo.id !== id
+    })
+    renderList()
+}
+
+function editFunc(id) {
+    let todo = todoList.find((todo) => todo.id === id)
+    if (todo) {
+        userInput.value = todo.todo
+        btn.innerHTML = 'Edit'
+        editId = id
+    }
+}
